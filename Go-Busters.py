6e05a2fb3271363e9ae53df796d01e32ef7843f9
Stephen <s.guild2@students.clark.edu>
2020-04-22 21:59:58 -0700

    Programming Assignment week 1

---
+++ Go-Busters.py
@@ -0,0 +1,29 @@
+"""
+CTEC 121
+<Stephen>
+<Programming Assignment>
+<Second file>
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("    \\\     //     ")
+    print("     \\\___//      ")
+    print("     /     \      ")
+    print("    / 0<|>0 \     ")
+    print("   ( ___^___ )    ")
+    print("/---|  ___  |---\ ")
+    print("| | | /   \ | |  |")
+    print("| | | \ _ / | |  |")
+    print("\_!_|  \_/  |_!_/ ")
+    print(" |_||       ||_|  ")
+    print(" /_|\_______/|_\  ")
+    print("|__| \|___|/ |__| ")
+    print("Usada Lettuce")
+
+main()    
---
+++ This is a Test.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+<Stephen Guild>
+<Programming Assignment>
+<First file>
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("Warning")
+    print("This is not a test")
+    print("Do not adjust your sets")
+
+main()    
